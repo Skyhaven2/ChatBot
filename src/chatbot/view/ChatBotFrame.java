b3cbb75c087feba797a03268a4bafbe3cd96ca77
Camron Micheal Heaps <camron.heaps@gmail.com>
2014-10-21 13:43:43 -0600

    TextArea made

--- src/chatbot/view/ChatBotFrame.java
+++ src/chatbot/view/ChatBotFrame.java
@@ -7,18 +7,23 @@ import chatbot.controller.ChatBotController;
 public class ChatBotFrame extends JFrame
 {
 	private ChatBotPanel basePanel;
-	
+
 	public ChatBotFrame(ChatBotController baseController)
 	{
 		basePanel = new ChatBotPanel(baseController);
-		
+
 		setupFrame();
 	}
-	
+
+	/**
+	 * Sets up Frame properties. The first thing it does is sets up the panel
+	 * inside it by calling the ChatBotPanel method in ChatBotPanel Class.
+	 */
 	private void setupFrame()
 	{
 		this.setContentPane(basePanel);
 		this.setSize(600, 600);
+		this.setResizable(false);
 		setVisible(true);
 	}
 
--- src/chatbot/view/ChatBotPanel.java
+++ src/chatbot/view/ChatBotPanel.java
@@ -1,11 +1,9 @@
 package chatbot.view;
 
 import java.awt.Color;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SpringLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 import chatbot.controller.ChatBotController;
 
@@ -13,43 +11,72 @@ public class ChatBotPanel extends JPanel
 {
 	private ChatBotController baseController;
 
-	private JButton firstButton;
-	private JTextField firstTextField;
+	private JButton InputButton;
+	private JTextField InputTextField;
 	private SpringLayout baseLayout;
+	private JTextArea chatArea;
+	private JScrollPane chatPane;
 
 	public ChatBotPanel(ChatBotController baseController)
 	{
 		this.baseController = baseController;
 
-		firstButton = new JButton("click the button... it is so clicky :D");
-		firstTextField = new JTextField(25);
+		InputButton = new JButton("Enter");
+		InputTextField = new JTextField(45);
 		baseLayout = new SpringLayout();
+		chatArea = new JTextArea(30, 45);
+		chatPane = new JScrollPane(chatArea);
 
+		setupPane();
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
 
+	private void setupPane()
+	{
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+	}
+	
+	/**
+	 * Sets up the Panel inside the Frame. Adds objects into the panel.
+	 */
 	private void setupPanel()
 	{
 		this.setBackground(Color.CYAN);
 		this.setSize(600, 600);
 		this.setLayout(baseLayout);
-		this.add(firstButton);
-		this.add(firstTextField);
+		this.add(InputButton);
+		this.add(InputTextField);
+		this.add(chatPane);
 	}
 
+	/**
+	 * Sets up the location for all of the objects.
+	 */
 	private void setupLayout()
 	{
-		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 1, SpringLayout.NORTH, firstButton);
-		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 10, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -10, SpringLayout.SOUTH, this);
-		baseLayout.putConstraint(SpringLayout.EAST, firstButton, -10, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, InputButton, -1, SpringLayout.NORTH, InputTextField);
+		baseLayout.putConstraint(SpringLayout.SOUTH, InputTextField, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, InputButton, 5, SpringLayout.EAST, InputTextField);
+		baseLayout.putConstraint(SpringLayout.WEST, InputTextField, 20, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, chatPane, 20, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, chatPane, 20, SpringLayout.WEST, this);
 	}
 
+	/**
+	 * Sets up listeners for interactive objects.
+	 */
 	private void setupListeners()
 	{
-
+		InputButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				InputTextField.setText(InputTextField.getText()+" :)");
+			}
+		});
 	}
 
 }
